70a60a8526255286601703e4f1a7c64c5bda2dfc
Uzair Aj <53966756+UzairAJ1@users.noreply.github.com>
2023-02-18 20:36:32 +0500

    sad

--- src/components/certificates/Certificate.js
+++ src/components/certificates/Certificate.js
@@ -20,7 +20,7 @@ const Certificate = () => {
             <div className='container mx-auto w-full h-screen flex flex-col justify-center items-center z-20 gap-4'>
 
                 <h2 className='mt-20 text-center text-2xl uppercase font-semibold text-gray-500 tracking-[11px] hover:tracking-[2px] hover:duration-500 duration-2000 cursor-pointer'>Certificates</h2>
-                <div className='relative w-[600px] h-[600px]'>
+                <div className='relative md:w-[600px] md:h-[600px] w-[400px] h-[400px]'>
                     <Image src={freecodecamp} />
                 </div>
 
